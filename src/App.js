2119de6f74c83f771bd40f9e4f5dd4b029301f3a
Emma Smith <esmith@flextrade.com>
2017-10-20 12:55:16 +0100

    Just a simple calculator.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import calculator from './calculator.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div id="App">
-        <header id="App-header">
-          <img src={calculator} id="App-logo" alt="logo" />
-          <h1 className="App-title">A Tool to Calculate.</h1>
-        </header>
         <div id="TheCalculator">
           <div id="TheScreen">
             <div className="screenText resultText">Placeholder</div>
