42f9a3f8e90079eee452670cc8a250aab42a3d69
Emma Smith <esmith@flextrade.com>
2017-10-20 11:14:15 +0100

    Change logo and header.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import calculator from './calculator.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div id="App">
+        <header id="App-header">
+          <img src={calculator} id="App-logo" alt="logo" />
+          <h1 className="App-title">A Tool to Calculate.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
